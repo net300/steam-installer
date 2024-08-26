2c35368f57b2cb8c28b7895c4862c5e1b72716f5
net300 <net3.0@outlook.com>
2024-08-26 13:56:00 +0800

    Add files via upload
    the sourse code

---
+++ steam_installer.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+#include <iostream>
+int main()
+{
+    std::cout << "install the steam?" << std::endl;
+    std::cin.get();
+    system("winget install --id Valve.Steam -i");
+    return 0;
+}
