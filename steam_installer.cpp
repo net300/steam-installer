f3cf1d7af8a584ea46c0450dd4b56c15e0bfc6cc
net300 <net3.0@outlook.com>
2024-08-30 11:50:04 +0800

    Update steam_installer.cpp

--- steam_installer.cpp
+++ steam_installer.cpp
@@ -2,8 +2,17 @@
 #include <iostream>
 int main()
 {
-    std::cout << "install the steam?" << std::endl;
-    std::cin.get();
-    system("winget install --id Valve.Steam -i");
+    std::cout << "install the steam?(Y/N)";
+    char input;
+    std::cin >> input;
+    bool Yes;
+    Yes = (input == ("Y" or "y"));
+    if (Yes)
+        system("winget install --id Valve.Steam -i");
+    else
+        std::cout << "what will you install";
+        char input2;
+        std::cin >> input2;
+        system("winget install "+input2);
     return 0;
 }
